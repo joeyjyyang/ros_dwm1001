45094979ba378eb6ebb6b25271ca61f40b97be7f
Joey Yang <joeyyang.ai@gmail.com>
2021-01-04 14:33:24 +0000

    associated each anchor with one (fixed) ROS topic

--- scripts/dwm1001_node.py
+++ scripts/dwm1001_node.py
@@ -43,8 +43,6 @@ tag = {
 def init():
     global serialPortDWM1001
 
-    # Create anchor objects.
-    createAnchors()
     # close the serial port in case the previous run didn't closed it properly
     serialPortDWM1001.close()
     # sleep for one sec
@@ -91,17 +89,19 @@ def set_rate():
     rospy.loginfo("Set UWB publish rate to 100 ms or 10 Hz")
     time.sleep(1)
 
-def createAnchors():
+def createAnchors(anchor_data):
   global anchors, num_anchors
-   
+
   for index in range(num_anchors):
+    offset = index * 6 # 0->0, 1->6, 2->12, 3->18
+    
     anchor = {
       #"anchor_number" : anchor_data[0 + offset],
-      "id" : 0,
-      "x" : 0,
-      "y" : 0,
-      "z" : 0,
-      "distance" : 0
+      "id" : anchor_data[1 + offset],
+      "x" : float(anchor_data[2 + offset]),
+      "y" : float(anchor_data[3 + offset]),
+      "z" : float(anchor_data[4 + offset]),
+      "distance" : float(anchor_data[5 + offset])
     }
     anchors.append(anchor)
   rospy.loginfo("Created anchors.")
@@ -118,30 +118,40 @@ def parseSerial(raw_line):
       
     # Check if serial data is valid based on total length and number of anchors.
     if len(line) == valid_length and int(line[1]) == num_anchors:
-	rospy.loginfo(line)
-
         anchor_data_index = 2
         anchor_data_length = num_anchors * 6
         anchor_data = line[anchor_data_index : anchor_data_index + anchor_data_length]
+   
+	# Create anchors if not created.
+	if not anchors:
+	    createAnchors(anchor_data)
+ 	
+	else:
+            # Update Anchor objects.
+            for index in range(num_anchors):
+          	offset = index * 6 # 0->0, 1->6, 2->12, 3->18
     
-        # Update Anchor objects.
-        for index in range(num_anchors):
-          offset = index * 6 # 0->0, 1->6, 2->12, 3->18
-      
-          anchors[index]["id"] = anchor_data[1 + offset]
-          anchors[index]["x"] = float(anchor_data[2 + offset])
-          anchors[index]["y"] = float(anchor_data[3 + offset])
-          anchors[index]["z"] = float(anchor_data[4 + offset])
-          anchors[index]["distance"] = float(anchor_data[5 + offset])
+		# Iterate through Anchor objects.
+		for anchor in anchors:
+		    # Find matching Anchor by ID.
+		    if anchor["id"] == anchor_data[1 + offset]:
+          	        anchor["x"] = float(anchor_data[2 + offset])
+         	        anchor["y"] = float(anchor_data[3 + offset])
+         		anchor["z"] = float(anchor_data[4 + offset])
+          		anchor["distance"] = float(anchor_data[5 + offset])
 
-        tag_data_index = anchor_data_index + anchor_data_length
-        tag_data = line[tag_data_index : ]
+		    else:
+		        #rospy.logwarn("Anchor ID not found. Are you sure this Anchor has been created properly?")
+			pass
 
-        # Update Tag object.
-        tag["x"] = float(tag_data[1])
-        tag["y"] = float(tag_data[2])
-        tag["z"] = float(tag_data[3])
-        tag["quality_factor"] = float(tag_data[4])
+       	    tag_data_index = anchor_data_index + anchor_data_length
+            tag_data = line[tag_data_index : ]
+
+            # Update Tag object.
+            tag["x"] = float(tag_data[1])
+            tag["y"] = float(tag_data[2])
+            tag["z"] = float(tag_data[3])
+            tag["quality_factor"] = float(tag_data[4])
 
 def publishData():
   global serialPortDWM1001, anchors, tag, tag_msg, anchor_msgs, tag_pub, anchor1_pub, anchor2_pub, anchor3_pub, anchor4_pub
@@ -152,29 +162,31 @@ def publishData():
   # Parse raw line.
   parseSerial(serial_line)
 
-  # Populate ROS messages.
-  time_stamp = rospy.get_rostime()
+  # Ensure Anchor objects are created first.
+  if anchors:
+    # Populate ROS messages.
+    time_stamp = rospy.get_rostime()
  
-  tag_msg.header.stamp = time_stamp
-  tag_msg.x = tag["x"]
-  tag_msg.y = tag["y"]
-  tag_msg.z = tag["z"]
-  tag_msg.quality_factor = tag["quality_factor"]
+    tag_msg.header.stamp = time_stamp
+    tag_msg.x = tag["x"]
+    tag_msg.y = tag["y"]
+    tag_msg.z = tag["z"]
+    tag_msg.quality_factor = tag["quality_factor"]
 
-  for index in range(num_anchors):
-    #anchor_msgs[index].header.stamp = time_stamp
-    anchor_msgs[index].id = anchors[index]["id"]
-    anchor_msgs[index].x = anchors[index]["x"]
-    anchor_msgs[index].y = anchors[index]["y"]
-    anchor_msgs[index].z = anchors[index]["z"]
-    anchor_msgs[index].distance = anchors[index]["distance"]
+    for index in range(num_anchors):
+      anchor_msgs[index].header.stamp = time_stamp
+      anchor_msgs[index].id = anchors[index]["id"]
+      anchor_msgs[index].x = anchors[index]["x"]
+      anchor_msgs[index].y = anchors[index]["y"]
+      anchor_msgs[index].z = anchors[index]["z"]
+      anchor_msgs[index].distance = anchors[index]["distance"]
 
-  # Publish UWB data to ROS.
-  tag_pub.publish(tag_msg)
-  anchor1_pub.publish(anchor_msgs[0]) 
-  anchor2_pub.publish(anchor_msgs[1])
-  anchor3_pub.publish(anchor_msgs[2])
-  anchor4_pub.publish(anchor_msgs[3])
+    # Publish UWB data to ROS.
+    tag_pub.publish(tag_msg)
+    anchor1_pub.publish(anchor_msgs[0]) 
+    anchor2_pub.publish(anchor_msgs[1])
+    anchor3_pub.publish(anchor_msgs[2])
+    anchor4_pub.publish(anchor_msgs[3])
   
 def cleanup():
     global serialPortDWM1001
